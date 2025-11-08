b847d9d5f8c77ee8b9fb4b608f0ada5795824164
inequitas <tribalforce@gmail.com>
2025-11-08 19:42:52 +0100

    done for the day

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from stats import count_words
+from stats import count_words,count_letters
 
 def main():
     output = count_words(get_book_text("books/frankenstein.txt"))
     print(f"Found {output} total words")
+    county = count_letters(get_book_text("books/frankenstein.txt"))
+    print(f"Also {county} total letters")
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
--- stats.py
+++ stats.py
@@ -4,3 +4,11 @@ def count_words(input):
     for word in new_list:
         counter += 1
     return counter
+
+def count_letters(input):
+    set_of_letters = set()
+    for letter in input:
+        set_of_letters.add(letter.lower())
+    for word in set_of_letters:
+        
+    return set_of_letters
